98feb129662beb8f42dbd2f43e787421748352eb
Vanessa <machado.vanessa32@gmail.com>
2022-02-09 11:54:37 -0300

    validacoes do form

--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -1,3 +1,43 @@
+export function valida(input) {
+	const tipoDeInput = input.dataset.tipo;
+
+	if (validadores[tipoDeInput]) {
+		validadores[tipoDeInput](input);
+	}
+
+	if (input.valid) {
+		input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
+	} else {
+		input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
+	}
+
+    const mensagensDeErro = {
+    nome:{
+       valueMissing: 'o campo nome nao pode estar vazio',
+    },
+    email:{
+        valueMissing: 'O campo de e-mail nao pode estar vazio',
+        typeMismatch: ''
+    },
+    senha: {
+        valueMissing: 'O campo de senha nao pode estar vazio',
+        patternMismatch: 'A senha deve conter 6 a 12 caracteres, deve conter pelo menos letra maiúscula, um numero e nao deve conter símbolos.'
+    },
+    dataNascimento:{
+        valueMissing: 'O campo de data de nascimento nao pode estar vazio.',
+        customError: 'voce deve ser maior de 18 anos para se cadastrar.'
+    }
+              
+}
+
+
+
+const validadores = {
+	dataNascimento: (input) => validaDataNascimento(input),
+};
+
 const dataNascimento = document.querySelector('#nascimento');
 
 dataNascimento.addEventListener('blur', (evento) => {
